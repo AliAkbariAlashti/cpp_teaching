917e03f100aef039a1deecab8321e7394121e9c1
ALI <143089764+20ali05@users.noreply.github.com>
2024-10-25 17:09:40 +0330

    Create for_loop.cpp

---
+++ codes/for_loop.cpp
@@ -0,0 +1,21 @@
+// for loop
+#include <iostream>
+using namespace std;
+
+
+//for (initialization; condition; increment) {
+// the code
+//}
+
+
+
+// sum of 1 to 10
+int main (){
+    int sum = 0;
+    for (int i = 1; i <= 10; i++) {
+        sum += i;
+}
+    cout << "Sum: " << sum << endl;
+
+
+}
