3ae9195072550821ad4326c7db7264522a7a7f57
ALI <143089764+20ali05@users.noreply.github.com>
2024-10-25 17:14:52 +0330

    Create while.cpp

---
+++ codes/while.cpp
@@ -0,0 +1,17 @@
+// while loop
+#include <iostream>
+using namespace std;
+
+//while (condition) {
+    // the code
+//}
+
+// show numbers 1 to 5
+int main (){
+    int i = 1;
+    while (i <= 5) {
+        std::cout << i << std::endl;
+        i++;
+}
+}
+
