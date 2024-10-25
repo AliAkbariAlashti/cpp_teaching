7cab7e92249c31156201bd2ea5db2089624c8717
ALI <143089764+20ali05@users.noreply.github.com>
2024-10-25 17:18:26 +0330

    Create factorial.cpp

---
+++ exercise/factorial.cpp
@@ -0,0 +1,14 @@
+// factorial
+#include <iostream>
+using namespace std;
+
+int main (){
+    int number = 5;
+    int factorial = 1;
+    for (int i = 1; i <= number; i++) {
+        factorial *= i;
+        }
+    cout << "Factorial of " << number << " is " << factorial << endl;
+
+}
+
