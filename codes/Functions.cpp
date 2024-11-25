c210d26d0aabe60d3a2b178c0b0ee03254e7c2ba
ALI <143089764+20ali05@users.noreply.github.com>
2024-11-25 22:31:28 +0330

    Create Functions.cpp

---
+++ codes/Functions.cpp
@@ -0,0 +1,33 @@
+#Syntax
+void myFunction() {
+  // code to be executed
+}
+
+
+#Call a Function
+// Create a function
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction(); // call the function
+  return 0;
+}
+
+// Outputs "I just got executed!"
+
+
+#Function declaration
+void myFunction();
+
+// The main method
+int main() {
+  myFunction();  // call the function
+  return 0;
+}
+
+// Function definition
+void myFunction() {
+  cout << "I just got executed!";
+}
