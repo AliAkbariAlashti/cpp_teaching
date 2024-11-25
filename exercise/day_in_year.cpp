bcc28e836505d799a5c027da74c8cc3b49e0ced8
ALI <143089764+20ali05@users.noreply.github.com>
2024-11-25 22:04:14 +0330

    Update day_in_year.cpp

--- exercise/day_in_year.cpp
+++ exercise/day_in_year.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+string weekday_founder(int day) {
+    int month, day_in_month;
+    if (day <= 186) {
+        month = (day - 1) / 31 + 1;
+        day_in_month = (day - 1) % 31 + 1;
+    } else {
+        day -= 186;
+        month = (day - 1) / 30 + 7;
+        day_in_month = (day - 1) % 30 + 1;
+    }
+    return "Month: " + to_string(month) + ", Day: " + to_string(day_in_month);
+}
+
+int main() {
+    int day;
+    cout << "Enter the day number of the year (1-365): ";
+    cin >> day;
+
+    if (day >= 1 && day <= 365) {
+        cout << weekday_founder(day) << endl;
+    } else {
+        cout << "Invalid day number. Please enter a value between 1 and 365." << endl;
+    }
+
     return 0;
 }
