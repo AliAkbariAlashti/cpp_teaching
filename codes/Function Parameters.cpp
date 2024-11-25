186171ea8ed96c20c146abfbefb379d97267f2fa
ALI <143089764+20ali05@users.noreply.github.com>
2024-11-25 22:34:34 +0330

    Create Function Parameters.cpp

---
+++ codes/Function Parameters.cpp
@@ -0,0 +1,34 @@
+#Parameters and Arguments
+#Syntax
+void functionName(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+#Example
+void myFunction(string fname) {
+  cout << fname << " Refsnes\n";
+}
+
+int main() {
+  myFunction("Liam");
+  myFunction("Jenny");
+  myFunction("Anja");
+  return 0;
+}
+
+// Liam Refsnes
+// Jenny Refsnes
+// Anja Refsnes
+
+#Return Values
+#Example
+int myFunction(int x) {
+  return 5 + x;
+}
+
+int main() {
+  cout << myFunction(3);
+  return 0;
+}
+
+// Outputs 8 (5 + 3)
+
